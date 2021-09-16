b6a47525973c835cd7ee1d44fb3d14233b673ee2
Renato Oliveira <renato.printf@gmail.com>
2021-09-16 00:09:12 -0300

    Home page finished

--- letmeask/src/components/Button.tsx
+++ letmeask/src/components/Button.tsx
@@ -1,15 +1,14 @@
-import { useState } from "react";
+import { ButtonHTMLAttributes } from "react";
+import '../styles/button.scss'
 
-export function Button(){
-  const [counter, setCounter] = useState(0);
 
-  function increment(){
-    setCounter(counter + 1);
-  }
-  
+type ButtonProps =  ButtonHTMLAttributes<HTMLButtonElement>;
+
+
+export function Button(props: ButtonProps){
   return (
-    <button onClick = {increment}>
-        {counter}
-    </button>
+    <button className="button" {...props} />
   )
-}
+} 
+
+<Button />
--- letmeask/src/pages/Home.tsx
+++ letmeask/src/pages/Home.tsx
@@ -2,30 +2,33 @@ import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
 
+import '../styles/auth.scss';
+import {Button} from '../components/Button';
+
 export function Home(){
   return(
-    <div>
+    <div id="page-auth">
       <aside>
         <img src={illustrationImg} alt="Illustration symbolizing asking and questions" />
         <strong>Crie salas de Q&amp;A ao-vivo</strong>
         <p>Tire as dúvidas de sua audiência em tempo-real</p>
       </aside>
       <main>
-        <div>
+        <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
-          <button>
+          <button className="create-room">
             <img src={googleIconImg} alt="Logo do Google" />
             Crie sua sala com o Google
           </button>
-          <div>Ou entre em uma sala</div>
+          <div className="separator">Ou entre em uma sala</div>
           <form >
             <input 
               type="text"
               placeholder="Digite o código da sala" 
             />
-            <button type="submit">
+            <Button type="submit">
               Entrar na sala
-            </button>
+            </Button>
           </form>
         </div>
       </main>

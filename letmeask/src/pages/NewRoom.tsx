40737e355e17862183df8508ab9929d270696277
Renato Oliveira <renato.printf@gmail.com>
2021-09-26 22:36:07 -0300

    Initiating second part

--- letmeask/src/pages/NewRoom.tsx
+++ letmeask/src/pages/NewRoom.tsx
@@ -1,3 +1,4 @@
+import { FormEvent, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 import illustrationImg from '../assets/images/illustration.svg'
@@ -10,6 +11,15 @@ import { useAuth } from '../hooks/UseAuth'
 
 export function NewRoom (){
   //const { user } = useAuth()
+  const [newRoom, setNewRoom] = useState('');
+
+  async function handleCreateRoom(event: FormEvent){
+      event.preventDefault();
+
+      console.log(newRoom)
+  }
+
+
   return(
     <div id="page-auth">
       <aside>
@@ -22,10 +32,12 @@ export function NewRoom (){
         <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
           <h2>Criar uma nova sala</h2>
-          <form >
+          <form onSubmit={handleCreateRoom}>
             <input 
               type="text"
               placeholder="Nome da sala" 
+              onChange= {event =>setNewRoom(event.target.value)}
+              value = {newRoom}
             />
             <Button type="submit">
               Criar sala

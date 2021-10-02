b7937986c26cac18b23626e89929cf77160b1e4d
Renato Oliveira <renato.printf@gmail.com>
2021-10-02 16:43:09 -0300

    Comments session in progress

--- letmeask/src/components/RoomCode.tsx
+++ letmeask/src/components/RoomCode.tsx
@@ -2,13 +2,21 @@ import copyImg from '../assets/images/copy.svg';
 
 import '../styles/room-code.scss'
 
-export function RoomCode() {
+type RoomCodeProps = {
+  code: string;
+}
+
+export function RoomCode(props: RoomCodeProps) {
+  function copyRoomCodeToClipBoard() {
+    navigator.clipboard.writeText(props.code)
+  }
+
   return (
-    <button className="room-code">
+    <button className="room-code" onClick= {copyRoomCodeToClipBoard}>
       <div>
         <img src={copyImg} alt="Copy room code" />
       </div>
-      <span>Sala #21378234</span>
+      <span>Sala #{props.code}</span>
     </button>
   )
 }
--- letmeask/src/pages/Room.tsx
+++ letmeask/src/pages/Room.tsx
@@ -1,16 +1,55 @@
-import logoImg from "../assets/images/logo.svg"; 
+import { FormEvent, useState } from 'react';
+import { useParams } from 'react-router-dom'
 
+import logoImg from "../assets/images/logo.svg"; 
 import { Button } from "../components/Button"; 
 import { RoomCode } from "../components/RoomCode";
+import { useAuth } from '../hooks/UseAuth';
+import { database } from '../services/firebase';
+
+
 import '../styles/room.scss';
 
+type RoomParams= {
+  id: string;
+}
+
 export function Room(){
+  const {user} = useAuth();
+  const params= useParams<RoomParams>();
+  const [newQuestion, setNewQuestion] = useState('');
+  const roomId = params.id;
+
+  async function handleSendQuestion(event: FormEvent) {
+    event.preventDefault();
+
+    if(newQuestion.trim() === '') {
+      return;
+    }
+
+    if(!user){
+      throw new Error('You must be logged in.')
+    }
+
+    const question = {
+      content: newQuestion,
+      author: {
+        name: user.name,
+        avatar: user.avatar
+      },
+      isHighlighted: false,
+      isAnswered: false
+    };
+
+    await database.ref(`rooms/${roomId}/questions`).push(question)
+  }
+
   return (
     <div id="page-room">
       <header>
         <div className="content">
           <img src={logoImg} alt="letmeask" />
-          <RoomCode />
+          <RoomCode code={roomId} />
         </div>
       </header>
 
@@ -20,13 +59,15 @@ export function Room(){
           <span>4 perguntas</span>
         </div>
 
-        <form>
+        <form onSubmit={handleSendQuestion}>
           <textarea 
             placeholder="O que você quer perguntar?"
+            onChange={event => setNewQuestion(event.target.value)}
+            value= {newQuestion}
           />
           <div className="form-footer">
             <span>Para enviar uma pergunta, <button>faça seu login</button>.</span>
-            <Button type="submit">Enviar pergunta</Button>
+            <Button type="submit" disabled={!user}>Enviar pergunta</Button>
           </div>
         </form>
       </main>
